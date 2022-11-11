dc102240904c1e8834845c401f984d639dec2dae
Rainer Simon <rainer@rainersimon.io>
2022-11-11 13:34:16 +0100

    Fixed load indicator position on mobile

--- src/components/LoadIndicator/LoadIndicator.jsx
+++ src/components/LoadIndicator/LoadIndicator.jsx
@@ -1,11 +1,16 @@
+import { Size, useDeviceState } from '@peripleo/peripleo';
 import { CgSpinner } from 'react-icons/cg';
 
 import './LoadIndicator.css';
 
 export const LoadIndicator = props => {
 
+  const device = useDeviceState();
+
   return (
-    <div className="kima-load-indicator p6o-controls-btn">
+    <div 
+      className={device.size === "DESKTOP" ? 
+        "kima-load-indicator p6o-controls-btn" : "kima-load-indicator p6o-controls-btn mobile"}>
       <CgSpinner />
     </div>
   )
