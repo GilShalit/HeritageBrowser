ee376f7125d445af0e9ff9760d993fb9cd092bde
Rainer Simon <rainer@rainersimon.io>
2022-12-09 20:45:05 +0100

    Info panel

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
+import { CgArrowsExpandRight } from 'react-icons/cg';
+import { TbCurrentLocation } from 'react-icons/tb';
+import { FiMenu } from 'react-icons/fi';
 import Peripleo, { 
   AggregationsOverlay,
   Controls,
@@ -71,14 +74,50 @@ const App = () => {
             
             <MyLocationControl />
             
-            {/* <InfoControl popup={
-              <div className="kima-info-modal">
-                <h1>Welcome</h1>
-                <p>
-                  Welcome to the NLI Heritage Browser Prototype.
-                </p>
-              </div>
-            } /> */}
+            <InfoControl className="kima-welcome">
+              <h1>Welcome to the Heritage Browser!</h1>
+
+              <p>
+                Discover the digitally available treasures of the National Library of Israel 
+                displayed on a map according to where they were created or locations they describe.
+              </p>
+
+              <ul>
+                <li>
+                  Move the map around by dragging it and use the buttons on the right or your mouse to zoom in and out.
+                </li>
+                <li>
+                  Click on a place to see the number of items related to it, as well as links to wikidata 
+                  and the Kima Gazetteer. Another click will open a list of related items. 
+                </li>
+                <li>
+                  After opening an item, use the <CgArrowsExpandRight /> icon to enlarge the image. Click Details to view the 
+                  item's details at the library's website.
+                </li>
+                <Desktop>
+                  <li>
+                    Use the left bar to filter the types of items that interest you, or change the bar 
+                    from Record Types to Relationship Types to filter items according to their relation to the place.
+                  </li>
+                </Desktop>
+                <Mobile>
+                  <li> 
+                    Click the <FiMenu /> icon to filter the types of items that interest you, or change the bar 
+                    from Record Types to Relationship Types to filter items according to their relation to the place.
+                  </li>
+                </Mobile>
+                <li>
+                  The <TbCurrentLocation /> button on the right of the screen will focus the map on your current location. 
+                </li>
+              </ul>
+
+              <p>
+                Created by <a href="mailto:sinai.rusinek@mail.huji.ac.il">Sinai 
+                Rusinek</a>, <a href="mailto:gil.shalit@gmail.com">Gil Shalit</a> and <a href="mailto:hello@rainersimon.io">Rainer 
+                Simon</a> and was a finalist in the <a href="https://tarboot.org/" target="_blank">tarboot</a> 
+                competition's general track.
+              </p>
+            </InfoControl>
 
             {loading && 
               <LoadIndicator />
