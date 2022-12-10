39fa8d162f1d2b818e13971d2eefc5043069e675
Rainer Simon <rainer@rainersimon.io>
2022-12-10 19:02:30 +0100

    Minor style tweak

--- src/index.jsx
+++ src/index.jsx
@@ -14,7 +14,7 @@ import Peripleo, {
   MyLocationControl,
   HeatmapLayer,
   ZoomControl
-} from '@peripleo/peripleo';
+} from '@peripleo/peripleo/src';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { KimaPopup, KimaStore, KimaTooltip, LoadIndicator } from './components';
 import { TYPE_COLORS } from './Colors';
